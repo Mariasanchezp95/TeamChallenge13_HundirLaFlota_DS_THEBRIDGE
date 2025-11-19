1fa75167729a25dff9444198773f5fa63c06e3d6
Alejandro Torrero <m.alejandrotorrerosc@gmail.com>
2025-11-19 20:46:16 +0100

    variables

---
+++ variables.py
@@ -0,0 +1,22 @@
+# Variables Hundir la flota 
+
+Tablero_Dim = 10
+
+Agua = " "
+Barco = "O"
+Tocado = "X"
+Fallo = "-"
+
+Barcos_esloras = {
+    "lancha": 1,     
+    "caza": 2,        
+    "fragata": 3,     
+    "acorazado": 4  
+}
+
+Barcos_cantidad = {
+    "lancha": 4,   
+    "caza": 3,       
+    "fragata": 2,     
+    "acorazado": 1    
+}
